31c6cc740e29fe66d7169a482040c82bdead4736
Thomas Levine <_@thomaslevine.com>
2015-10-08 12:13:37 -0400

    run a web server and test

--- test_bottle_leveldb.py
+++ test_bottle_leveldb.py
@@ -1,13 +1,25 @@
+import threading, time
+
+import requests
 from bottle import Bottle
+
 from bottle_leveldb import Plugin
 
-fake_leveldb = 'Pretend this is a LevelDB.'
+def test_pass_through():
+    fake_leveldb = 'Pretend this is a LevelDB.'
 
-def test_bottle_leveldb():
     app = Bottle()
     ldb = Plugin(fake_leveldb, keyword = 'leveldb')
     app.install(ldb)
 
-    @app.route('/abc')
-    def abc(leveldb):
+    @app.route('/')
+    def index(leveldb):
         assert leveldb == fake_leveldb
+        return fake_leveldb
+
+    t = threading.Thread(None, target = app.run, daemon = True)
+    t.start()
+    time.sleep(1)
+    r = requests.get('http://127.0.0.1:8080/')
+    assert r.ok
+    assert r.text == fake_leveldb

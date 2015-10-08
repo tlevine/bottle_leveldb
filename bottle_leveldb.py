e5c9a5201966d4bd2eecbf0001eefda389f8d2cd
Thomas Levine <_@thomaslevine.com>
2015-10-08 11:43:14 -0400

    space

--- bottle_leveldb.py
+++ bottle_leveldb.py
@@ -1,8 +1,14 @@
+import inspect
+
 class Plugin(object):
     name = 'leveldb'
     api = 2
+
     def setup(self, app):
+        pass
+
     def apply(self, callback, route):
+        return
         args = inspect.getargspec(context['callback'])[0]
         if keyword not in args:
             return callback
